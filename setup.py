6ab07654bcfbc7ad9510631e9c1beddfe5257e4b
Hiroki Kondo <kompiro@gmail.com>
2014-03-16 10:17:03 +0000

    Specify install version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='IdobataPlugin', version='0.3',
+    name='IdobataPlugin', version='0.4',
     packages=find_packages(exclude=['*.tests*']),
     author='Hiroki Kondo',
     author_email='kompiro@gmail.com',
@@ -9,6 +9,7 @@ setup(
     description='Trac - Idobata integration',
     platforms='all',
     license='Apache License v2',
+    install_requires = ['Trac >= 1.0'],
     entry_points = {
         'trac.plugins': [
             'idobata = idobata.notification',
